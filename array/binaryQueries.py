74d2608d58cdadf0d8f78a79da2d84f98719d8da
Singham <rahul.singh4800@gmail.com>
2019-12-17 00:26:13 +0530

    binaryqueries

--- array/binaryQueries.py
+++ array/binaryQueries.py
@@ -10,12 +10,21 @@ ODD
 """
 
 def binaryQueries():
-    n, q = input().split()
-    value = [int(q) for q in input().split()]
-    
-
-
-
+    n, q = map(int, input().split())
+    l = input().split()
+    for i in range(0,q):
+        k = input().split()
+        print(k)
+        if k[0] == '0':
+            if l[int(k[2])-1] == '1':
+                print("ODD")
+            else:
+                print("EVEN")
+        if k[1] == '1':
+            if l[int(k[1]) - 1] == '0':
+                l[int(k[1]) - 1] = '1'
+            else:
+                '0'
 
 if __name__ == '__main__':
     binaryQueries()
